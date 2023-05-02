955f11363e7e5dcc4a21de1dea18b95651e01318
Greg K <gkreisman_cep@qq.com>
2023-05-02 19:02:38 +0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,64 @@
+// from the genuary generative poem
+// https://www.datamuse.com/api/
+// using other data sets
+const isAlpha = str => /^[a-zA-Z]*$/.test(str);
+
+// great regex is alpha for string check
+// the api fails on 
+let poemStart;
+let sl =0
+let sz =20
+let foo = new p5.Speech();
+let sent = "";
+
+
+function setup() {
+  cnv =createCanvas(600, 600);
+  cnv.parent("sketch-holder");
+  background(0);
+  poemStart = createInput('a');
+  poemStart.changed(writePoem);
+  createButton("generate").mousePressed(writePoem)
+  fill(255)
+  textSize(sz)
+ 
+  
+}
+
+function writePoem() {
+  // just runs once to start the poem
+   background(0);
+  // clear sentence for speak
+  sent = "";
+  sl =0
+  
+  text(poemStart.value(),400, sl*sz+sz)
+  // build the sentence to speak
+  sent += (poemStart.value())+" ";
+  
+  print("bing")
+  sl++
+
+  getWordList(poemStart.value())
+  
+}
+
+function getWordList(prompt) {
+  // getting next word frequent followeres
+  loadJSON(`https://api.datamuse.com/words?lc=${prompt}&sp=*`, showword);
+}
+
+function showword(words){
+  //print(words)
+  let choice = random(words)
+  print(choice.word)
+  text(choice.word,400, sl*sz+sz)
+  sent += (choice.word)+" "
+  sl++
+  if (sl <20&& choice.word != "." && choice.word !='i'  && isAlpha(choice.word) ) {
+    getWordList(choice.word.trim())
+    
+  }else{
+    foo.speak(sent)
+  }
+}
